4b9583d5f90d351c8903245eb53f17168f499399
Ayşegül Öztürk <69517016+aysegul-ozturk@users.noreply.github.com>
2025-03-30 22:15:20 +0300

    Update layout.js

--- web/src/app/layout.js
+++ web/src/app/layout.js
@@ -4,15 +4,22 @@ import "./globals.css";
 import { BrowserRouter } from 'react-router-dom';
 import { BasketProvider } from './context/BasketContext';
 import AppTheme from './shared-theme/AppTheme'
+import { FavoritesProvider } from './context/FavoritesContext';
 
 
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body >
+      <body>
+        <BrowserRouter>
           <BasketProvider>
-          {children}
+            <FavoritesProvider>
+              <AppTheme>
+                {children}
+              </AppTheme>
+            </FavoritesProvider>
           </BasketProvider>
+        </BrowserRouter>
       </body>
     </html>
   );
