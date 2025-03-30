a7ffeab425f3a2a9c632126724f90f856bb18596
Ayşegül Öztürk <69517016+aysegul-ozturk@users.noreply.github.com>
2025-03-30 22:15:43 +0300

    Update page.js

--- web/src/app/page.js
+++ web/src/app/page.js
@@ -1,22 +1,21 @@
 "use client";
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import { Route, Routes } from 'react-router-dom';
 import SignIn from './sign-in/SignIn';
 import SignUp from './sign-up/SignUp';
 import MarketingPage from './home-page/MarketingPage';
-import AppTheme from './shared-theme/AppTheme'
+import FavoritesPage from './favorites/page';
+import MyBasket from './components/MyBasket';
 
 function App() {
   return (
-    <AppTheme>
-    <Router>
-      <Routes>
-        <Route path="/sign-in" element={<SignIn />} />
-        <Route path="/sign-up" element={<SignUp />} />
-        <Route path="/home-page" element={<MarketingPage />} /> {/* Home page */}
-        <Route path="*" element={<MarketingPage />} /> {/* Default route to SignIn */}
-      </Routes>
-    </Router>
-    </AppTheme>
+    <Routes>
+      <Route path="/sign-in" element={<SignIn />} />
+      <Route path="/sign-up" element={<SignUp />} />
+      <Route path="/home-page" element={<MarketingPage />} /> {/* Home page */}
+      <Route path="/favorites" element={<FavoritesPage />} /> {/* Favorites page */}
+      <Route path="/my-basket" element={<MyBasket />} />
+      <Route path="*" element={<MarketingPage />} /> {/* Default route to SignIn */}
+    </Routes>
   );
 }
 
